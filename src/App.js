a3f1761528335076fbe00624450dafc6b081401b
Boburjon200609 <baxtiyorovboburjon22@gmail.com>
2024-10-12 08:23:37 +0500

    start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Cart from './components/cart/cart';
 
 function App() {
   return (
     <div className="App">
        <i class="fa-solid fa-bars"></i>
+       <Cart/>
 
     </div>
   );
--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -6,7 +6,7 @@ export default function Cart() {
     <div className='cart'>
         <div className='box'>
             <div className='h1'>
-                <h1>Digital marketing that delivers results</h1>
+                <h1  style={{marginTop:"35px"}}>Digital marketing that delivers results</h1>
             </div>
             <div className='lorem'>
                 <p>We work by a set of key values which has naturally produced an amazing environment for talent to flourish and for our clients to get the best work from us.</p>
