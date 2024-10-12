7ac2e45e0e848dc2f2bb800f673e11f9a1b05ce3
Boburjon200609 <baxtiyorovboburjon22@gmail.com>
2024-10-12 08:11:30 +0500

    hello

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -6,16 +6,24 @@ export default function Cart() {
     <div className='cart'>
         <div className='box'>
             <div className='h1'>
-                <h1>Digital marketing that delivers results</h1>
+                <h1 style={{marginTop:"35px"}}>Digital marketing that delivers results</h1>
             </div>
             <div className='lorem'>
                 <p>We work by a set of key values which has naturally produced an amazing environment for talent to flourish and for our clients to get the best work from us.</p>
             </div>
-            <div className='button'></div>
+            <div className='button'>
+                <button style={{width:"220px",height:"70px,",border:"none",borderRadius:"22px",color:"white",fontSize:"16px"}}>CONTACT US</button>
+            </div>
 
 
         </div>
-        <div className='box1'></div>
+        <div className='box1'>
+
+        </div>
+        <div className='text'>
+        <h1 style={{width:"100%",marginLeft:'670px',marginTop:"-100px",fontSize:"19px"}}>Image from <span style={{color:'rgb(71, 201, 86)',}} >Freepik</span></h1>
+        </div>
+
 
 
       
