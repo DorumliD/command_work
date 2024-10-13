c90fb37ebe77c8897fff2182ad74d658ec99db06
Boburjon200609 <baxtiyorovboburjon22@gmail.com>
2024-10-13 22:10:21 +0500

    font_size

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -5,6 +5,8 @@ export default function Cart() {
   return (
     <div className='cart'>
         <div className='box'>
+
+
             <div className='h1'>
                 <h1 style={{marginTop:"35px"}}>Digital marketing that delivers results</h1>
             </div>
@@ -12,7 +14,7 @@ export default function Cart() {
                 <p>We work by a set of key values which has naturally produced an amazing environment for talent to flourish and for our clients to get the best work from us.</p>
             </div>
             <div className='button'>
-                <button style={{width:"220px",height:"65px,",border:"none",borderRadius:"22px",color:"white",fontSize:"16px"}}>CONTACT US</button>
+                <button style={{width:"220px",height:"65px,",border:"none",borderRadius:"22px",color:"white",fontSize:"16px"}}><a style={{textDecoration:"none",color:"white"}} href='https://website1284624.nicepage.io/Home.html?version=71b9431f-2f21-406e-9ba8-cad91144db95#'>CONTACT US</a></button>
             </div>
 
 
