0c51c2b1bce49984bc3d874fcb9f6b920665a942
shahribonu24 <mijoz6226@gmail.com>
2024-10-16 14:28:39 +0500

    mistake

--- src/components/shein/Shein.js
+++ src/components/shein/Shein.js
@@ -6,7 +6,7 @@ function Shein() {
   return (
     <div className='bigDiv'>
       
-      <div style={{width:'500px',height:'450px', backgroundColor:'white', paddingTop: '59px', paddingLeft:'40px', paddingRight:'40px', gap:'40px'}}>
+      <div style={{width:'500px',height:'400px', backgroundColor:'white', paddingTop: '59px', paddingLeft:'40px', paddingRight:'40px', gap:'40px'}}>
       <h1><b>Customized Digital Marketing Service</b></h1><br/>
       <p>Timed she his law the spoil round defer. In surprise concerns informed betrayed he learning is ye. Ignorant formerly so ye blessing. He as spoke avoid given downs money on we. Of properly carriage shutters ye as wandered up repeated moreover</p>
       
