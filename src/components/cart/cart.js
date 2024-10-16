c0f34837475bf6978358a3653fffd3f6195568b3
Boburjon200609 <baxtiyorovboburjon22@gmail.com>
2024-10-17 02:26:01 +0500

    let

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -14,7 +14,7 @@ export default function Cart() {
 
 
             <div className='h1'>
-                <h1 style={{marginTop:"35px"}}>Digital marketing that delivers results</h1>
+                <h1 style={{marginTop:"70px"}}>Digital marketing that delivers results</h1>
             </div>
             <div className='lorem'>
                 <p>We work by a set of key values which has naturally produced an amazing environment for talent to flourish and for our clients to get the best work from us.</p>
@@ -28,7 +28,7 @@ export default function Cart() {
         <div className='box1'>
             <div id="animatedDiv" className='img'></div>
             <div className='text'>
-                <p>Image from <span style={{color:"rgb(71, 201, 86)"}}>Freepik</span></p>
+                <p style={{marginLeft:"25%",fontSize:"20px"}}>Image from <span style={{color:"rgb(71, 201, 86)"}}>Freepik</span></p>
             </div>
            
 
